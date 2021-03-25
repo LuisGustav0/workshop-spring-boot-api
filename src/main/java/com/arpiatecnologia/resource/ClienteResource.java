f163ba37d35fc153a4de6dcdb38349f5d378c22f
luisgustavo <luis.gustav0@live.com>
2021-03-25 18:45:22 -0300

    refactor: alteração ClienteResource adicionando um service para salvar

--- src/main/java/com/arpiatecnologia/resource/ClienteResource.java
+++ src/main/java/com/arpiatecnologia/resource/ClienteResource.java
@@ -1,6 +1,8 @@
 package com.arpiatecnologia.resource;
 
 import com.arpiatecnologia.model.Cliente;
+import com.arpiatecnologia.service.ClienteService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/cliente")
 public class ClienteResource {
+
+    private final ClienteService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente) {
-        return cliente;
+        return this.service.save(cliente);
     }
 }
---
+++ src/main/java/com/arpiatecnologia/service/ClienteService.java
@@ -0,0 +1,8 @@
+package com.arpiatecnologia.service;
+
+import com.arpiatecnologia.model.Cliente;
+
+public interface ClienteService {
+
+    Cliente save(Cliente cliente);
+}
