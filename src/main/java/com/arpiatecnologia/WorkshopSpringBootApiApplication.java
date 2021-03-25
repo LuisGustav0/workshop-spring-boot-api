b57930842c26cbadf67b12d8a558c79f952cc7fc
luisgustavo <luis.gustav0@live.com>
2021-03-25 18:01:42 -0300

    chore: add projeto incial spring framework

---
+++ src/main/java/com/arpiatecnologia/WorkshopSpringBootApiApplication.java
@@ -0,0 +1,12 @@
+package com.arpiatecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootApiApplication.class, args);
+	}
+}
