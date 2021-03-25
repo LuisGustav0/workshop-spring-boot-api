834381947ce0ad6b668e54e52137b40c8373a6b6
luisgustavo <luis.gustav0@live.com>
2021-03-25 20:07:20 -0300

    refactor: ClienteService e ClienteServiceImpl com boa pratica

--- src/main/java/com/arpiatecnologia/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/arpiatecnologia/service/impl/ClienteServiceImpl.java
@@ -1,12 +1,19 @@
 package com.arpiatecnologia.service.impl;
 
 import com.arpiatecnologia.model.Cliente;
+import com.arpiatecnologia.repository.ClienteRepository;
 import com.arpiatecnologia.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class ClienteServiceImpl implements ClienteService {
+
+    private final ClienteRepository repository;
 
     @Override
     public Cliente save(Cliente cliente) {
-        return null;
+        return this.repository.save(cliente);
     }
 }
