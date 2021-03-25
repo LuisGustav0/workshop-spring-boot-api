451fcb3b789a25228d6d3740e6525d657162aef7
luisgustavo <luis.gustav0@live.com>
2021-03-25 19:38:01 -0300

    feat: add ClienteServiceImpl implementando metodo save

---
+++ src/main/java/com/arpiatecnologia/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,12 @@
+package com.arpiatecnologia.service.impl;
+
+import com.arpiatecnologia.model.Cliente;
+import com.arpiatecnologia.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        return null;
+    }
+}
