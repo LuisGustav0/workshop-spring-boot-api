94eb2171b88a7416e3d664a25f4bec3c0e298206
luisgustavo <luis.gustav0@live.com>
2021-03-25 19:08:33 -0300

    refactor: alteração ClienteResource com boa pratica

--- src/main/java/com/arpiatecnologia/resource/ClienteResource.java
+++ src/main/java/com/arpiatecnologia/resource/ClienteResource.java
@@ -2,13 +2,18 @@ package com.arpiatecnologia.resource;
 
 import com.arpiatecnologia.model.Cliente;
 import com.arpiatecnologia.service.ClienteService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +22,16 @@ public class ClienteResource {
 
     private final ClienteService service;
 
+    private URI getLocation(@NonNull Long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(id).toUri();
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente) {
-        return this.service.save(cliente);
+    public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+        Cliente clienteSaved = this.service.save(cliente);
+        URI uriLocation = this.getLocation(clienteSaved.getId());
+
+        return ResponseEntity.created(uriLocation).body(clienteSaved);
     }
 }
