a556b217d62736ad0518f26706679928ae00856e
luisgustavo <luis.gustav0@live.com>
2021-03-25 23:17:29 -0300

    refactor: add anotação para banco de dados

--- src/main/java/com/arpiatecnologia/model/Cliente.java
+++ src/main/java/com/arpiatecnologia/model/Cliente.java
@@ -1,16 +1,31 @@
 package com.arpiatecnologia.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cliente")
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
+    @Column(name = "nome", length = 100, nullable = false)
     private String nome;
 }
