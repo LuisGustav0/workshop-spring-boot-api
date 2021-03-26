f23638beb0345f9a4d654022902f984c4bd751d2
luisgustavo <luis.gustav0@live.com>
2021-03-25 23:14:09 -0300

    test: melhoria mensagem de negocio

--- src/test/java/com/arpiatecnologia/resource/ClienteResourceTest.java
+++ src/test/java/com/arpiatecnologia/resource/ClienteResourceTest.java
@@ -18,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static com.arpiatecnologia.consts.ClienteErrorConst.CLIENTE_NOME_JA_CADASTRADO;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -93,19 +94,16 @@ class ClienteResourceTest {
     @Test
     @DisplayName("Deve lançar erro ao tentar criar um cliente com nome que já possui")
     void create_ClienteComNomeDuplicado() throws Exception {
-
         Cliente fakeCliente = this.fakeNewCliente("Luis Gustavo");
         String json = this.mapper.writeValueAsString(fakeCliente);
 
-        String message = "Nome já cadastrado";
-
-        given(this.service.save(any(Cliente.class))).willThrow(new BusinessException(message));
+        given(this.service.save(any(Cliente.class))).willThrow(new BusinessException(CLIENTE_NOME_JA_CADASTRADO));
 
         MockHttpServletRequestBuilder request = this.mockPost(json);
 
         this.mockMvc.perform(request)
                     .andExpect(status().isBadRequest())
                     .andExpect(jsonPath("errors", hasSize(1)))
-                    .andExpect(jsonPath("errors[0].message").value(message));
+                    .andExpect(jsonPath("errors[0].message").value(CLIENTE_NOME_JA_CADASTRADO));
     }
 }
