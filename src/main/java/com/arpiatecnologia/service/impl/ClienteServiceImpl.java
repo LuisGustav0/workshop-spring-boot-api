c1be4e588bf91474733e8308b1dcdf960c35ed92
luisgustavo <luis.gustav0@live.com>
2021-03-25 23:16:13 -0300

    refactor: melhoria na impl do ClienteService

--- src/main/java/com/arpiatecnologia/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/arpiatecnologia/service/impl/ClienteServiceImpl.java
@@ -1,10 +1,13 @@
 package com.arpiatecnologia.service.impl;
 
+import com.arpiatecnologia.exception.BusinessException;
 import com.arpiatecnologia.model.Cliente;
 import com.arpiatecnologia.repository.ClienteRepository;
 import com.arpiatecnologia.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.arpiatecnologia.consts.ClienteErrorConst.CLIENTE_NOME_JA_CADASTRADO;
 
 @Service
 @RequiredArgsConstructor
@@ -12,8 +15,15 @@ public class ClienteServiceImpl implements ClienteService {
 
     private final ClienteRepository repository;
 
+    public boolean existsByNome(String nome) {
+        return this.repository.existsByNome(nome);
+    }
+
     @Override
     public Cliente save(Cliente cliente) {
+        if (this.existsByNome(cliente.getNome()))
+            throw new BusinessException(CLIENTE_NOME_JA_CADASTRADO);
+
         return this.repository.save(cliente);
     }
 }
